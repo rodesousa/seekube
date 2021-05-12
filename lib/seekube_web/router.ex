189b7b9827d7b7b20f7f8bda160ea6cd6bfa85bf
rodesousa <dessroberto@gmail.com>
2021-05-12 16:18:53 +0200

    add json endpoint
    Signed-off-by: rodesousa <dessroberto@gmail.com>

--- lib/seekube_web/router.ex
+++ lib/seekube_web/router.ex
@@ -17,6 +17,7 @@ defmodule SeekubeWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/documents", DocumentController, except: [:new, :edit]
   end
 
   # Other scopes may use custom stacks.
